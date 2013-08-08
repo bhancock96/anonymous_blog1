3c6a23a13d6cdd922aebdbd314e88067c7ec5140
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 09:14:22 -0500

    added migrations, models, post view, create view

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,27 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/create' do
+
+  erb :create
+end
+
+get 'post/:id' do
+end
+
+get '/tags' do
+end
+
+
+
+# ********** POST ROUTES ****************
+
+post '/create' do
+end
+
+post '/post/:id/edit' do
+end
+
+post '/post/:id/delete' do
+end
