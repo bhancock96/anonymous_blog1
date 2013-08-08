0a6a415bcf7e596ae5bebab21d2ee636e2190c1a
dbc-apprentice <apprentice@devbootcamp.com>
2013-08-08 09:32:47 -0500

    added post and posts view, added post and tag creation

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -8,10 +8,17 @@ get '/create' do
   erb :create
 end
 
-get 'post/:id' do
+get '/post/:id' do
+  @post = Post.find(params[:id])
+  erb :post
 end
 
 get '/tags' do
+end
+
+get '/posts' do
+  @posts = Post.all
+  erb :posts
 end
 
 
@@ -19,6 +26,10 @@ end
 # ********** POST ROUTES ****************
 
 post '/create' do
+  @post = Post.create(title: params[:post][:title], body: params[:post][:body])
+  @post.tags.create(name: params[:post][:tags])
+  @post.save
+  redirect '/posts'
 end
 
 post '/post/:id/edit' do
